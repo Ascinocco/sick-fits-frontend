a1e55ce024bf65fbf509d905de9c55be07705876
Anthony Scinocco <scinocco.a@gmail.com>
2018-12-06 19:58:01 -0500

    added signout functionality

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "edfa6cd1faf1bdf135cb";
+/******/ 	var hotCurrentHash = "e2afaf4b871f879c9a67";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
