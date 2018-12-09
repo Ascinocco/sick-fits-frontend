22e2d9d156e1136d336b72c38bd14b62b3add0ca
Anthony Scinocco <scinocco.a@gmail.com>
2018-12-09 08:48:48 -0500

    added Permission component and query

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a50186a3ac5150cf0496";
+/******/ 	var hotCurrentHash = "aa033743ce2447f16d84";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
