6afd4ec02ace4d4e06aef956f69bfa2c386e1f93
Anthony Scinocco <scinocco.a@gmail.com>
2018-12-09 10:06:57 -0500

    implemented cart component on the frontend

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2259bbf953f3f31bffc8";
+/******/ 	var hotCurrentHash = "57a81a1f4a157f931f18";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
